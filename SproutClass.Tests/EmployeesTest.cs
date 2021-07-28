7d43a4b7c7cd4645af58ed056f31bf5fed723cd1
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-07-28 12:01:29 -0500

    inital commit

---
+++ SproutClass.Tests/EmployeesTest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SproutClass.Tests
+{
+    public class EmployeesTest
+    {
+        [Fact]
+        public void GetEmployeesData_ReturnsSalaries()
+        {
+            var currentDataObject = new Mock<IEmployeeService>();
+            currentDataObject.Setup(x => x.GetEmployeesData()).Returns(new List<Employee> (){ new Employee {UniqueId=1,Salary=100 } });
+            var salaryData = currentDataObject.Object.GetEmployeesData();
+                     
+            Assert.Contains(salaryData, x => x.Salary > 0);
+        }
+    }
+}
